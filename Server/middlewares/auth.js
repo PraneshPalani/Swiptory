acf9a64a4b67cb247c11fe9d8460760630ca45b9
PraneshPalani <pranesh2778@gmail.com>
2026-04-13 20:53:31 +0530

    Update auth.js

--- Server/middlewares/auth.js
+++ Server/middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const isAuth = (req, res, next) => {
-  const token = req.cookies.token;
+  const token = req.headers.authorization?.split(" ")[1];
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
   }
